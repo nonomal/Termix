a320d6cfbf2be8e480659c52c467c5fb34695a35
LukeGus <bugattiguy527@gmail.com>
2025-08-20 01:01:02 -0500

    Fixed formatting

--- src/MobileApp.tsx
+++ src/MobileApp.tsx
@@ -3,15 +3,15 @@ import {TerminalComponent} from "@/ui/mobile/Apps/Terminal/TerminalComponent.tsx
 export function MobileApp() {
     return (
         <div>
-                <TerminalComponent
-                    hostConfig={{
-                        ip: "192.210.197.55",
-                        port: 22,
-                        username: "root",
-                        password: "bugatti$123",
-                        authType: "password",
-                    }}
-                />
+            <TerminalComponent
+                hostConfig={{
+                    ip: "192.210.197.55",
+                    port: 22,
+                    username: "root",
+                    password: "bugatti$123",
+                    authType: "password",
+                }}
+            />
         </div>
     )
 }
